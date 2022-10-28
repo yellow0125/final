c7b57a5d0373469c41757031612dc1c5d8d7fc0f
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-10-27 23:31:49 -0700

    setup database firebase

---
+++ myapp/firebase/firebase-setup.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId
+} from "react-native-dotenv"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
---
+++ myapp/firebase/firestore.js
@@ -0,0 +1,3 @@
+import { collection, addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { firestore } from "./firebase-setup";
+
