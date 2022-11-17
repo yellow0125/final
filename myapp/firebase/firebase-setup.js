8e56af54adaab11ecbce098b4e723c772b95599a
Sophia Wu <114020121@link.cuhk.edu.cn>
2022-11-17 11:10:38 -0800

    install redux related package

--- myapp/firebase/firebase-setup.js
+++ myapp/firebase/firebase-setup.js
@@ -12,14 +12,24 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: apiKey,
+//   authDomain: authDomain,
+//   projectId: projectId,
+//   storageBucket: storageBucket,
+//   messagingSenderId: messagingSenderId,
+//   appId: appId,
+// };
+
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
-};
+  apiKey: "AIzaSyAccitAphJm637huwnPR9eDE_V_UIsj4xY",
+authDomain : "project-1100779858743235788.firebaseapp.com",
+projectId : "project-1100779858743235788",
+storageBucket : "project-1100779858743235788.appspot.com",
+messagingSenderId : "926625345465",
+appId : "1:926625345465:web:a9a2b9d42e3e7882779068",
+measurementId : "G-8W81QC9GCW",
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
