7b78a97256e7610792d5acd786a57b23aa7120c0
yellow0125 <huang.cui@northeastern.edu>
2022-11-24 22:23:52 -0800

    Update EditProfile.js

--- myapp/screens/EditProfile.js
+++ myapp/screens/EditProfile.js
@@ -58,28 +58,39 @@ export default function EditProfile({ navigation, route }) {
             <Column>
                 <Input label="Username" value={username} f_onChange={setUsername} />
 
-                <Picker
-                    label="Gender"
-                    selectedValue={gender}
-                    onValueChange={(itemValue) =>
-                        setGender(itemValue)
-                    }>
-                    <Picker.Item label="Male" value="Male" />
-                    <Picker.Item label="Female" value="Female" />
-                    <Picker.Item label="Hiden" value="Hiden" />
-                </Picker>
+                <View style={styles.pickerContainer}>
+                    <Text style={styles.pickerLabel}>Gender</Text>
+                    <Picker
+                        label="Gender"
+                        selectedValue={gender}
+                        onValueChange={(itemValue) =>
+                            setGender(itemValue)
+                        }>
+                        <Picker.Item label="Please Select" value="default" />
+                        <Picker.Item label="Male" value="Male" />
+                        <Picker.Item label="Female" value="Female" />
+                        <Picker.Item label="Hiden" value="Hiden" />
+                    </Picker>
+                </View>
 
-                <Picker
-                    label="Location"
-                    selectedValue={location}
-                    mode={'dropdown'}
-                    onValueChange={(itemValue) =>
-                        setLocation(itemValue)
-                    }>
-                    <Picker.Item label="China" value="China" />
-                    <Picker.Item label="Japan" value="Japan" />
-                    <Picker.Item label="Italy" value="Italy" />
-                </Picker>
+                <View style={styles.pickerContainer}>
+                    <Text style={styles.pickerLabel}>Location</Text>
+                    <Picker
+                        label="Location"
+                        selectedValue={location}
+                        mode={'dropdown'}
+                        onValueChange={(itemValue) =>
+                            setLocation(itemValue)
+                        }>
+                        <Picker.Item label="Please Select" value="default" />
+                        <Picker.Item label="China" value="China" />
+                        <Picker.Item label="Japan" value="Japan" />
+                        <Picker.Item label="Italy" value="Italy" />
+                    </Picker>
+                </View>
+
+
+
 
             </Column>
             <Row style={styles.buttonsContainer}>
@@ -114,4 +125,11 @@ const styles = StyleSheet.create({
         borderColor: "#B7B7B7",
         height: 50,
     },
+    pickerContainer: {
+        marginLeft: 20
+    },
+    pickerLabel: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
 });
