7c147117fc3a105d9e1b06c637e7d75c7ee0be7c
Sophia Wu <114020121@link.cuhk.edu.cn>
2022-12-03 18:58:54 -0800

    add permission request in ios

--- myapp/components/NotificationManager.js
+++ myapp/components/NotificationManager.js
@@ -1,13 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import * as Notification from "expo-notifications"
+import * as Notifications from "expo-notifications"
 import MainButton from './UI/MainButton'
 
 export default function NotificationManager() {
     const name = "Test Notification"
+
+    const verifyPermission = async () => {
+      const permissionStatus = await Notifications.getPermissionsAsync();
+      if (permissionStatus.granted) {
+        return true;
+      }
+      const requestedPermission = await Notifications.requestPermissionsAsync({
+        ios: {
+          allowBadge: true,
+        },
+      });
+      return requestedPermission.granted;
+    };
+  
+
     const scheduleNotificationHandler = async () => {
         try {
-            await Notification.scheduleNotificationAsync({
+            const hasPermission = await verifyPermission();
+            if (!hasPermission) {
+              return;
+            }
+            await Notifications.scheduleNotificationAsync({
                 content: {
                     title: "You have a notification",
                     body: 'This is my firt local notification ${name}',
