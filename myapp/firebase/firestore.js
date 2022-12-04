08b59401570a8b2174c78a74bf1aa8a5ee22e8fc
yellow0125 <huang.cui@northeastern.edu>
2022-12-04 13:18:20 -0800

    Update firestore.js

--- myapp/firebase/firestore.js
+++ myapp/firebase/firestore.js
@@ -65,7 +65,7 @@ export const uploadRecipeToDB = async (recipe) => {
         await addDoc(collection(firestore, "recipes"), {
             ...recipe,
             user: auth.currentUser.uid,
-            like: 520,
+            like: 99,
 
         });
     } catch (error) {
